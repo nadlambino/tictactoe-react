48b42ae9546179d0093a5224b7948b3fef0f621d
Nad Lambino <ronaldlambino0000@gmail.com>
2023-04-12 18:54:44 +0800

    Remove unnecesary files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import { io } from 'socket.io-client';
 
 const URL = 'http://localhost';
@@ -14,5 +13,3 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
-
-reportWebVitals();
