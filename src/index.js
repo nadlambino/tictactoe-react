8dd20abbc1bb197c1d0bd45390b4b708ae00a5b4
Nad Lambino <ronaldlambino0000@gmail.com>
2023-04-12 18:16:39 +0800

    Set up socket connection

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { io } from 'socket.io-client';
+
+const URL = 'http://localhost';
+
+export const socket = io(URL);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
