1d4d6b61b2c7c8884c44ed129e111ae32af101e6
Nad Lambino <ronaldlambino0000@gmail.com>
2023-04-14 14:08:44 +0800

    Refactor UI

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,7 +25,7 @@ export default function Form({createRoomCallback}) {
     <>
       <div className='form-container'>
         <input type='text' placeholder='Username' value={username} onChange={(e) => handleUsernameChange(e.target.value)} />
-        <input type='text' placeholder='Room Name' onChange={(e) => handleRoomChange(e.target.value)} />
+        <input type='text' placeholder='Room ID' onChange={(e) => handleRoomChange(e.target.value)} />
         <div className='btn-container'>
           <button onClick={handleCreateRoom}>CREATE</button>
           <button>JOIN</button>
